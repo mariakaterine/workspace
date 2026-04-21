0254dd3d1b8b1a28244709b3078f1e2598339b51
mariakaterine <mariamunerar@ielacandelariamedellin.edu.co>
2026-04-21 21:10:53 +0000

    21-04

---
+++ 24-03/modulo1-1.js
@@ -0,0 +1,6 @@
+var nombre ="katerine";
+let edad=16;
+const cuidad ="medellin";
+console.log("mi nombre es "+nombre);
+console.log("mi edad es "+edad);
+console.log("mi cuidad es "+cuidad);
---
+++ 24-03/modulo1-5.js
@@ -0,0 +1,5 @@
+let incognita = 5;
+let y = 10;
+
+console.log(y);
+console.log(incognita);
